e68ae91744189656fb6f22215773b23030e7121a
pedrovotf <pedrovotf@gmail.com>
2023-02-20 16:17:43 -0300

    JS update, HTML update products, SASS update

--- JavaScript/mainJs.js
+++ JavaScript/mainJs.js
@@ -1,11 +1,84 @@
 
-//primeiro formulario
+//conexão
+let pageAPI = 1
+let page = document.body
+
+page.onload = () =>
+    begin() //inicia a procura
+        .then(connected) //se conectado
+        .catch(errorConnection) // se nao conectado
+
+function begin(){
+    return new Promise((resolve, reject) => { // protocolo padrao para conexão
+        
+        const xhttp = new XMLHttpRequest() // protocolo padrao para conexão
+
+        xhttp.onreadystatechange = function(){ // protocolo padrao para conexão
+            if (this.readyState == 4 && this.status == 200){  // protocolo padrao para conexão (valida)
+                const response = JSON.parse(this.responseText) // protocolo padrao para JSON
+
+                resolve(response) // protocolo padrao para resultado conexão
+
+            }
+
+            if(this.status === 404){ // protocolo padrao para conexão (invalida)
+                reject() // protocolo padrao para conexão
+            }
+        }
+
+        xhttp.open('GET',`https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${pageAPI}`) // Link e metodo
+
+        xhttp.send() // protocolo padrao para conexão
+
+    })
+}
+
+function errorConnection(){ //caso conexao falhe
+    return console.log('error') 
+}
+
+const htmlInsert = document.querySelector('.products-list') //cria lista para inserir produto
+
+function connected(response){ // se conexao der certo
+    const APIResponse = response.products // retira valores
+
+    for(let i = 0 ;i < APIResponse.length; i ++){ // insere texto baseado no tanto de produtos
+        htmlInsert.innerHTML = htmlInsert.innerHTML + `<div class="product-item">
+        <img id="image" src="${APIResponse[i].image}" alt="item from supplier">
+                    <h4 class="name-product">${APIResponse[i].name}</h4>
+                    <p class="description-product">${APIResponse[i].description}</p>
+                    <p class="value-product">De: ${APIResponse[i].oldPrice}</p>
+                    <p class="Value-descont">Por: ${APIResponse[i].price}</p>
+                    <p class="value-product-part">ou 2x de ${(APIResponse[i].price)/2}</p>
+                    <button class="buy-product">Comprar</button>
+        </div>`
+    }
+
+    return  
+
+}
+
+
+const btnNextPage = document.querySelector('.more-product')
+btnNextPage.addEventListener("click", (e) => {
+    e.preventDefault();
+   pageAPI += 1
+   begin()
+   console.log(pageAPI)
+   console.log(e)
+
+})
+
+
+//formularios
 
 let formulario_1 = document.querySelector('#subscribe')
 let formulario_2 = document.querySelector('#news-subscribe')
 
 formulario_1.onsubmit = function(evento){
     evento.preventDefault()// cancela comportamento padrao
+
+    testes = ``;
 
     let inputName = document.forms['subscribe']['nome'] // seleciona o campo
     let inputMail = document.forms['subscribe']['email']
